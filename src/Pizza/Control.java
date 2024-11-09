69a9fe5e9a2c8d36d7891c728afd7af53d5366f4
Ari Lei <leimingda0621@gmail.com>
2024-11-09 02:17:49 -0500

    finished pizza create, and page 1 gui

--- src/Pizza/Control.java
+++ src/Pizza/Control.java
@@ -5,5 +5,14 @@ import javafx.event.ActionEvent;
 public class Control {
     public void CreateKnow(ActionEvent actionEvent) {
     }
+
+    public void NYPizza(ActionEvent actionEvent) {
+    }
+
+    public void CHPizza(ActionEvent actionEvent) {
+    }
+
+    public void HandleDIY(ActionEvent actionEvent) {
+    }
 }
 
--- src/Pizza/Order.java
+++ src/Pizza/Order.java
@@ -5,4 +5,20 @@ import java.util.ArrayList;
 public class Order {
     private int number; //order number
     private ArrayList<Pizza> pizzas; //can use List<E> instead of ArrayList<E>
+    Order(ArrayList<Pizza> pizzas, int number) {
+        this.pizzas = pizzas;
+        this.number = number;
+    }
+    public int getNumber() {
+        return number;
+    }
+    public void setNumber(int number) {
+        this.number = number;
+    }
+    public ArrayList<Pizza> getPizzas() {
+        return pizzas;
+    }
+    public void setPizzas(ArrayList<Pizza> pizzas) {
+        this.pizzas = pizzas;
+    }
 }
